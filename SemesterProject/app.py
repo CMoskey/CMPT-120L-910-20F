40f8faedffc838eb68e00f82f43b686b7663793d
CMoskey <cameron.moskey@gmail.com>
2020-12-11 22:24:21 -0500

    Final Project

--- SemesterProject/app.py
+++ SemesterProject/app.py
@@ -1,17 +1,28 @@
+#Imports
 from flask import Flask
 from flask import render_template
-import jinja2
+import logging
 
 
+
+
+
+#logging 
+
+
+
+
+
+
+
+
+#WEBPAGES
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
-
     return render_template("index.html")
     
-
 @app.route('/elements')
 def elements():
     return render_template("elements.html")
@@ -23,3 +34,22 @@ def generic():
 @app.route('/lab10')
 def ten():
     return render_template("lab10.html")
+
+@app.route('/aboutme')
+def about():
+    return render_template("aboutme.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/personality')
+def personality():
+    return render_template("personality.html")
+
+
+#error handling
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
